3ad83f373a7e1283196b4609f1a5cb6d2a4227de
JanithaGit <janithaprashadkatukenda@gmail.com>
2021-04-24 18:31:29 +0530

    with register

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
-const express = require("express")
+const express = require("express");
+const path = require('path');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env'});
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("<h1>Janitha Home Page 123</h1>")
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+
+
+
+const publicDirectory = path.join(__dirname, './public');
+
+app.use(express.static(publicDirectory));
+
+//parsing URL-encoded bodies
+app.use(express.urlencoded({ extended: false}));
+
+app.use(express.json());
+
+app.set('view engine', 'hbs');
+
+db.connect((error) => {
+if(error){
+    console.log(error)
+
+}else{
+    console.log("MYSQL Connected....")
+}
 })
+
+//Define Routes
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
+
 
 app.listen(5000, () =>{
     console.log("Server Start on port 5000")
---
+++ controllers/auth.js
@@ -0,0 +1,46 @@
+const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+ 
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+exports.register = (req, res) => {
+    console.log(req.body);
+
+const {name, email, password, passwordConfirm } = req.body;
+
+db.query('SELECT email FROM users WHERE email = ?',[email], async(error, results ) =>{
+    if(error){
+        console.log(error);
+    }
+    if( results.length > 0 ){
+        return res.render('register', {
+            message: 'That email is alredy in use'
+        })
+    }
+    else if( password !== passwordConfirm){
+        return res.render('register', {
+            message: 'Password do not match'
+        });
+    }
+let hashedPassword = await bcrypt.hash(password, 8);
+console.log(hashedPassword);
+
+db.query('INSERT INTO users SET ? ',{name: name, email: email, password: hashedPassword, })
+if(error){
+console.log(error)
+}else
+{
+    console.log(results)
+    return res.render('register', {
+        message: 'User Registerd'
+    });
+}
+});
+
+}
